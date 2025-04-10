f32eefa65107d5428b636b54d296772ad1a95525
liron shtern <lironweiz@gmail.com>
2025-04-10 16:55:04 +0300

    עדכון קבצים וחיבור לפיירבייס

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,21 @@
-// תצטרכי להחליף את הערכים האלה עם הפרטים שתקבלי מקונסולת Firebase
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_ID",
-    appId: "YOUR_APP_ID"
-  };
-  
-  // אתחול Firebase
-  firebase.initializeApp(firebaseConfig);
-  const db = firebase.firestore();
+  apiKey: "AIzaSyCUNYnbB7Efkr3h3Lric9INjE7_PBn1VDQ",
+  authDomain: "game1-5e569.firebaseapp.com",
+  projectId: "game1-5e569",
+  storageBucket: "game1-5e569.firebasestorage.app",
+  messagingSenderId: "1039720116965",
+  appId: "1:1039720116965:web:418f8388a52663ca635a0c",
+  measurementId: "G-FK4YNH72H8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
